9089c3a2cc480ada2f65447ce3af79b7d62532b1
brendonstahl97 <brendonstahl97@gmail.com>
2020-11-19 14:09:59 -0500

    added post and delete functionality

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require("fs");
 
 let notes = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), (err) => {if(err) throw err})); 
-console.log(notes);
+let tempId = notes[notes.length - 1].id;
 
 // Express app
 const app = express();
@@ -31,14 +31,35 @@ app.get("/api/notes", (req, res) => {
 })
 
 app.post("/api/notes", (req, res) => {
+    req.body.id = tempId;
+    tempId++;
     notes.push(req.body);
-    res.sendFile(path.join(__dirname, "notes.html"));
+
+    updateNotes(res);
 })
 
+app.delete("/api/notes/:id", (req, res) => {
 
+    for (let i = 0; i < notes.length; i++) {
+        if(notes[i].id == req.params.id) {
+            notes.splice(i, 1);
+        }
+    }
+
+    updateNotes(res);
+
+})
 
 
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
+
+
+
+const updateNotes = (res) => {
+    fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(notes), (err) => {if(err) throw err});
+    res.sendFile(path.join(__dirname, "notes.html"));
+}
+
